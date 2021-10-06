83be4055a30daa8b1898d2adad54ac5802175754
AdelBagateev <90271115+AdelBagateev@users.noreply.github.com>
2021-10-06 17:30:19 +0400

    Update C1W2.java

--- cw1/2/C1W2.java
+++ cw1/2/C1W2.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class C2W2{
+public class C1W2{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt(), maxi = -999999, c = 0, c_final = 0;

ab970961cd1a5f791c584b10606f2324eaba680b
AdelBagateev <90271115+AdelBagateev@users.noreply.github.com>
2021-10-06 17:31:36 +0400

    Update C1W3.java

--- cw1/3/C1W3.java
+++ cw1/3/C1W3.java
@@ -1,10 +1,19 @@
-import java.io.*;
 import java.util.*;
+public class C1W3{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), maxi = -999999, c = 0, c_final = 0;
+		while(n != -1){
+			if (n%2==0){
+				c++;
+			}
+			if (n > maxi){
+				maxi = n;
+				c_final = c;
 
-public class C1W3 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // Решение задачи 3 следует писать здесь
-        System.out.println("Solve me!");
-    }
+			}
+			n = sc.nextInt();
+		}
+		System.out.println(maxi + " " + c_final);
+	}
 }

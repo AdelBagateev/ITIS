faf8ad0df692831fd50a45165c6449e7c4154bf3
AdelBagateev <90271115+AdelBagateev@users.noreply.github.com>
2021-10-06 17:30:52 +0400

    Update C1W2.java

--- cw1/2/C1W2.java
+++ cw1/2/C1W2.java
@@ -1,18 +1,1 @@
-import java.util.*;
-public class C1W2{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt(), maxi = -999999, c = 0, c_final = 0;
-		while(n != -1){
-			if (n%2==0){
-				c++;
-			}
-			if (n > maxi){
-				maxi = n;
-				c_final = c;
-			}
-			n = sc.nextInt();
-		}
-		System.out.println(maxi + " " + c_final);
-	}
-}
+ 

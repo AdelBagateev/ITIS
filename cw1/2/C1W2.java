80ca9e08c3d8af71fd23d009aa6e0568054eef06
AdelBagateev <90271115+AdelBagateev@users.noreply.github.com>
2021-10-06 17:29:40 +0400

    Update C1W2.java

--- cw1/2/C1W2.java
+++ cw1/2/C1W2.java
@@ -1,10 +1,18 @@
-import java.io.*;
 import java.util.*;
-
-public class C1W2 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        // Решение задачи 2 следует писать здесь
-        System.out.println("Solve me!");
-    }
+public class C2W2{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt(), maxi = -999999, c = 0, c_final = 0;
+		while(n != -1){
+			if (n%2==0){
+				c++;
+			}
+			if (n > maxi){
+				maxi = n;
+				c_final = c;
+			}
+			n = sc.nextInt();
+		}
+		System.out.println(maxi + " " + c_final);
+	}
 }
